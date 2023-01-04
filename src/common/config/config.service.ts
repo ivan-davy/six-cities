8c5c410d8503ac7e059e1fccb554cef915cd85a1
ivan-davy <imdavydov@outlook.com>
2023-01-04 16:23:25 +0300

    Реализует общую инфраструктуру приложения, вводит логгеры и сервисы

---
+++ src/common/config/config.service.ts
@@ -0,0 +1,29 @@
+import {ConfigInterface} from './config.interface.js';
+import {config} from 'dotenv';
+import {LoggerInterface} from '../logger/logger.interface.js';
+import {configSchema, ConfigSchemaType} from './config.schema.js';
+
+export default class ConfigService implements ConfigInterface {
+  private config: ConfigSchemaType;
+  private logger: LoggerInterface;
+
+  constructor(logger: LoggerInterface) {
+    this.logger = logger;
+
+    const parsedOutput = config();
+
+    if (parsedOutput.error) {
+      throw new Error('Can\'t read the .env file. Perhaps the file does not exist.');
+    }
+
+    configSchema.load({});
+    configSchema.validate({allowed: 'strict', output: this.logger.info});
+
+    this.config = configSchema.getProperties();
+    this.logger.info('.env file found and successfully parsed!');
+  }
+
+  public get<T extends keyof ConfigSchemaType>(key: T) {
+    return this.config[key];
+  }
+}
